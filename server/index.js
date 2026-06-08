d467db0055cead262970fdd5d656c2149e25ee05
Pedro Amarante <Pedro Amarante>
2026-06-07 16:40:36 -0800

    Make UI mobile first

--- server/index.js
+++ server/index.js
@@ -425,6 +425,20 @@ seedManagersAndLeague();
 seedFifaData();
 
 const app = express();
+app.use((req, res, next) => {
+  const origin = req.get("origin");
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
+  }
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  if (req.method === "OPTIONS") {
+    res.status(204).end();
+    return;
+  }
+  next();
+});
 app.use(express.json());
 
 app.post("/api/login", (req, res) => {
